f228494834d95d45b7e0e51be1995c0881395d8b
Matthew Collier <collier.matthew@gmail.com>
2016-08-09 10:22:36 -0400

    Namespace pages.

--- test/protractor/pages/index.js
+++ test/protractor/pages/index.js
@@ -1,6 +1,7 @@
 var pages = global.bedrock.pages || {};
 
-pages.messageList = require('./messageList');
-pages.message = require('./message');
+pages['bedrock-angular-messages'] = {};
+pages['bedrock-angular-messages'].messageList = require('./messageList');
+pages['bedrock-angular-messages'].message = require('./message');
 
 module.exports = global.bedrock.pages = pages;
