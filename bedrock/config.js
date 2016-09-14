f5b7417bd2d487046e1e6c85a70ea022710f5363
Dave Longley <dlongley@digitalbazaar.com>
2016-09-14 14:05:23 -0400

    Fix typo.

--- bedrock/config.js
+++ bedrock/config.js
@@ -3,17 +3,14 @@
  *
  * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
  */
-var fs = require('fs');
-var path = require('path');
-
 module.exports = function(bedrock) {
   // FIXME: overrides config set elsewhere
-  // export bedrock-key location for UI
+  // export bedrock-messages location for UI
   var vars = bedrock.config.views.vars;
   vars['bedrock-angular-messages'] = vars['bedrock-angular-messages'] || {};
   if('messages' in bedrock.config) {
     vars['bedrock-angular-messages'].endpoints =
-      bedrock.config.messages.endpoints || '';
+      bedrock.config.messages.endpoints || {};
   } else {
     vars['bedrock-angular-messages'].endpoints = {};
   }
