5eb9e89c841543af855ac816b783203461918230
Dave Longley <dlongley@digitalbazaar.com>
2016-09-14 14:33:25 -0400

    Require `bedrock-messages` when using bedrock config.

--- bedrock/config.js
+++ bedrock/config.js
@@ -3,15 +3,12 @@
  *
  * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
  */
+require('bedrock-messages');
+
 module.exports = function(bedrock) {
-  // FIXME: overrides config set elsewhere
   // export bedrock-messages location for UI
   var vars = bedrock.config.views.vars;
-  vars['bedrock-angular-messages'] = vars['bedrock-angular-messages'] || {};
-  if('messages' in bedrock.config) {
-    vars['bedrock-angular-messages'].endpoints =
-      bedrock.config.messages.endpoints || {};
-  } else {
-    vars['bedrock-angular-messages'].endpoints = {};
-  }
+  vars['bedrock-angular-messages'] = {
+    endpoints: bedrock.config.messages.endpoints
+  };
 };
